80b132e0472a35c2f587152f5ce632083003709d
MaxxusX <66540504+MaxxusX@users.noreply.github.com>
2024-05-08 14:40:11 -0500

    Update EEL.js

--- EEL.js
+++ EEL.js
@@ -137,8 +137,8 @@ EEL.parse(`
 // single-line comment
 /* also single-line comment :(
 -- more single-line comment
--- set str hello, world!
--- set num 1
+// tba: set str hello, world!
+// tba: set num 1
 print str
 print num
 // this should print "3 is 3!!!"
